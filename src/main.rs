8d80df077d54dc2921c12fd2d8e9ea379d50070d
mifour <dufour.thomas@hotmail.fr>
2021-04-09 13:40:08 +0200

    WIP: continue

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 extern crate clap;
 use clap::{Arg, App};
-use std::cmp;
+use std::cmp::min;
 use std::io::{self, BufRead};
+
+
+pub trait ModuloSignedExt {
+    fn modulo(&self, n: Self) -> Self;
+}
+macro_rules! modulo_signed_ext_impl {
+    ($($t:ty)*) => ($(
+        impl ModuloSignedExt for $t {
+            #[inline]
+            fn modulo(&self, n: Self) -> Self {
+                (self % n + n) % n
+            }
+        }
+    )*)
+}
+modulo_signed_ext_impl! { usize i8 i16 i32 i64 }
+
 
 fn is_int(s: String) -> Result<(), String> {
 	match s.parse::<i64>() {
@@ -76,19 +93,126 @@ fn validate_round(s: String) -> Result<(), String>{
 	match s.to_lowercase().as_str(){
 		"up"=> Ok(()),
 		"down"=> Ok(()),
-		"from-zero (default)"=> Ok(()),
+		"from-zero"=> Ok(()),
 		"towards-zero"=> Ok(()),
 		"nearest"=> Ok(()),
 		_ => Err(String::from("invalid round method"))
 	}
 }
 
+fn validate_si_suffix(s: &String) -> bool{
+	"KMGTPEZY".contains(s)
+}
+
+fn validate_eici_suffix(s: &String) -> bool{
+	"KiMiGiTiPiEiZiYi".contains(s)
+}
+
+fn get_si_power(s: &String)-> (i8, i64){
+	if ! validate_si_suffix(s){
+
+	}
+	let power = match s.as_str(){
+		"K" => 3,
+		"M" => 6,
+		"G" => 9,
+		"T" => 12,
+		"P" => 15,
+		"E" => 18,
+		"Z" => 21,
+		"Y" => 24,
+		_ => 0
+	};
+	(10, power.clone())
+}
+
+fn get_eic_power(s: &String) ->(i8, i64){
+	if ! (validate_si_suffix(s) || validate_eici_suffix(s)){
+
+	}
+	let power = match s.as_str(){
+		"K" => 10,
+		"Ki" => 10,
+		"M" => 20,
+		"Mi" => 20,
+		"G" => 30,
+		"Gi" => 30,
+		"T" => 40,
+		"Ti" => 40,
+		"P" => 50,
+		"Pi" => 50,
+		"E" => 60,
+		"Ei" => 60,
+		"Z" => 70,
+		"Zi" => 80,
+		"Y" => 80,
+		"Yi" => 80,
+		_ => 0
+	};
+	(2, power.clone())
+}
+
+fn get_auto_power(s: &String) -> (i8, i64){
+	if validate_si_suffix(s){
+		return get_si_power(s);
+	}
+	if validate_eici_suffix(s){
+		return get_eic_power(s);
+	}
+	(0, 0)
+}
+
+fn to_si_power(base: u8, power: i64) -> (String, i64){
+	let mut pow = power.clone();
+	if base == 2{
+		// 2**(10*x) == 10**(3*x) for IEC standarts
+		pow = (pow/10)*3;
+	}
+	match pow{
+		p if p >= 24 => {("Y".to_string(), p-24)},
+		p if p >= 21 => {("Z".to_string(), p-21)},
+		p if p >= 18 => {("E".to_string(), p-18)},
+		p if p >= 15 => {("P".to_string(), p-15)},
+		p if p >= 12 => {("T".to_string(), p-12)},
+		p if p >= 9 => {("G".to_string(), p-9)},
+		p if p >= 6 => {("M".to_string(), p-6)},
+		p if p >= 3 => {("K".to_string(), p-3)},
+		_ => {("".to_string(), pow)}
+	}
+}
+
+
+fn to_eic_power(eic_i: bool, base: u8, power: i64) -> (String, i64){
+	let mut pow = power.clone();
+	let i = match eic_i{
+		true => {"i"},
+		false => {""}
+	};
+	if base == 10{
+		// 2**(10*x) == 10**(3*x) for IEC standarts
+		pow = (pow/3)*10;
+	}
+	match pow{
+		p if p >= 80 => {("Y".to_string()+&i, p-80)},
+		p if p >= 70 => {("Z".to_string()+&i, p-70)},
+		p if p >= 60 => {("E".to_string()+&i, p-60)},
+		p if p >= 50 => {("P".to_string()+&i, p-50)},
+		p if p >= 40 => {("T".to_string()+&i, p-40)},
+		p if p >= 30 => {("G".to_string()+&i, p-30)},
+		p if p >= 20 => {("M".to_string()+&i, p-20)},
+		p if p >= 10 => {("K".to_string()+&i, p-10)},
+		_ => {("".to_string(), pow)}
+	}
+}
+
+
+
 fn main() {
-	// let arg_vec = vec![
-	// 	"debug", "delimiter", "field", "format", "from", "grouping", "header",
-	// 	"invalid", "padding", "round", "suffix", "to", "to-unit", "zero_terminated",
+	//  ToDOs:
+	// 	"debug", "delimiter", "field", "format", "from", "header",
+	// 	"invalid", "padding", "suffix", "to", "zero_terminated",
 	// 	"number"
-	// ];
+	// 
     let inputs = App::new("numfmt")
     	.version("0.1")
     	.author("mifour")
@@ -147,6 +271,7 @@ fn main() {
            .long("round")
            .value_name("METHOD")
            .help("ues METHOD for rounding when scaling among up, down, from-zero (default), towards-zero, nearest")
+           .validator(validate_round)
            .takes_value(true))
     	.arg(Arg::with_name("suffix")
            .long("suffix")
@@ -218,7 +343,7 @@ fn main() {
     	Some(value) => String::from(value),
     	// TODO:
     	// FIX stdin read
-    	None => io::stdin().lock().lines().map(|l| l.unwrap()).collect::<Vec<String>>().join("\n")
+    	None => io::stdin().lock().lines().map(|line| line.unwrap()).collect::<Vec<String>>().join("\n")
 	};
 	println!("NUMBER:{:?}", number);
 	if number.is_empty(){
@@ -231,25 +356,86 @@ fn main() {
     let mut header_end:usize = 0;
     if header > 0{
     	let indices: Vec<(usize, &str)> = number.match_indices("\n").collect();
-    	header_end  = indices[cmp::min(indices.len(), header)].0;
+    	header_end  = indices[min(indices.len(), header)].0;
     }
     let h_text = &number[..header_end];
     if !h_text.is_empty(){
     	println!("HEADERS: {:?}", h_text);
     }
     number = number[header_end..].to_string();
+
+    let mut power: i64 = 0;
+    let mut base: i8 = 0;
+    
+    if inputs.is_present("from"){
+    	let from = inputs.value_of("from").unwrap_or("auto");
+    	let res = match from.to_lowercase().as_str(){
+    		"si" => {
+    			get_si_power(&number)
+    		},
+    		"iec" => {
+    			get_eic_power(&number)
+    		},
+    		"iec-i" => {
+    			get_eic_power(&number)
+    		}
+    		_ => {
+    			get_auto_power(&number)
+    		}
+    	};
+    	base = res.0;
+    	power = res.1;
+
+    }
+    println!("{:?}", base);
+    println!("{:?}", power);
     
     
     
     // convert string to number
-    let mut res:i64 = number.parse::<i64>().unwrap();
+    let mut res:f64 = number.parse::<f64>().unwrap();
     // scale to unit_size
-    let unit_size = inputs.value_of("to-unit").unwrap_or("1")
-    	.parse::<i64>().unwrap();
+    let unit_size = inputs.value_of("to-unit").unwrap_or("1.0")
+    	.parse::<f64>().unwrap();
     res = res / unit_size;
+
+    if inputs.is_present("rounding"){
+    	match inputs.value_of("rouding").unwrap_or("from-zero").to_lowercase().as_str(){
+			"up"=> {
+				res = res.ceil();
+			},
+			"down"=> {
+				res = res.floor();
+			},
+			"from-zero"=> {
+				//away from-zero
+				res = res.trunc() + res.signum();
+			},
+			"towards-zero"=> {
+				res = res.trunc();
+			},
+			"nearest"=> {
+				res = res.round();
+			},
+			_ => {}
+		}
+    }
+    
+    let mut res = res.to_string();
     // convert to exporting format
     if inputs.is_present("grouping"){
-    	println!("ToDo");
+    	let res_str = res.clone();
+    	let (mut to_add, mut remain) = res_str.split_at(res_str.len().modulo(3));
+    	let mut res_vec = vec![to_add];
+    	while remain.len()>3{
+    		let x = remain.split_at(3);
+    		to_add = x.0;
+    		remain = x.1;
+    		res_vec.push(to_add.clone());
+    	}
+    	res_vec.push(remain.clone());
+    	res = res_vec.join(",");
+
     }
 
     println!("FINAL: {:?}", res);
